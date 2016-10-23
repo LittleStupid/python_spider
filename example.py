85f44c69730740d97d712128d836ae0b6e342a02
LittleStupid <lttlstpd@live.com>
2016-10-23 22:38:55 +0800

    search covers

--- example.py
+++ example.py
@@ -8,12 +8,12 @@ import time
 import re
 
 # timeout
-# socket.setdefaulttimeout(10)
+socket.setdefaulttimeout(6)
 
 #locate your phantomjs
 driver = webdriver.PhantomJS(executable_path='/usr/local/bin/phantomjs')
 
-def getCover(index)
+def getCover(index):
     driver.get("http://h.bilibili.com/dy" + str(index))
     time.sleep(3)
 
@@ -21,41 +21,26 @@ def getCover(index)
     bsObj = BeautifulSoup(pageSource, "html.parser")
     img = bsObj.find("img", {"style":"opacity: 1;"})
 
+    if img is None:
+        return
+    if img.attrs is None:
+        return
+    if img.attrs["src"] is None:
+        return
+
     try:
-        urlretrieve (image.attrs["src"], str(index)+'.jpg')
-    except FileNotFoundError:
-        print("file is not found")
-    except ValueError:
-        print("Value error")
-    except Exception:
-        print("Other error")
+        urlretrieve (img.attrs["src"], str(index)+'.jpg')
+    except Exception as e:
+        print(e)
 
-# driver.get("http://h.bilibili.com/dy6570")
-# time.sleep(3)
-# pageSource = driver.page_source
+def getAll(beginIndex, endIndex):
+    for i in range(beginIndex,endIndex):
+        try:
+            getCover(i)
+        except Exception as e:
+            print(e)
 
-# bsObj = BeautifulSoup(pageSource, "html.parser")
+first = (int)(argv[1])
+last = (int)(argv[2])
 
-# img = bsObj.find("img", {"style":"opacity: 1;"})
-# score = bsObj.find("div", {"title":"评分"})
-# names = bsObj.find("p", {"class":"name"})
-
-# print(img)
-# print("###")
-
-# print(score)
-print("###")
-
-# print(names)
-# driver.close()
-# time.sleep(5)
-
-
-# def getCover(index)
-#
-# def getAll(beginIndex, endIndex)
-#     for i in range(beginIndex,endIndex):
-#         try:
-#             getCover(i)
-#         except Exception as e:
-#             print(e)
+getAll(first,last)
---
+++ readme.md
@@ -0,0 +1,13 @@
+run this to get headers from [here](http://h.bilibili.com/)
+
+    python3 comic.py beginIndex endIndex
+
+Example:
+search headers form http://h.bilibili.com/dy10 to http://h.bilibili.com/dy100:
+
+    python3 comic.py 10 100
+
+--------------------------------
+search covers:
+
+    python3 example.py beginIndex endIndex
