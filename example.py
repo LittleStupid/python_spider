d2e74ce90b536228c5a2bf097ef9c37ed1451396
LittleStupid <lttlstpd@live.com>
2016-10-25 17:37:13 +0800

    you can search covers now

--- example.py
+++ example.py
@@ -13,34 +13,68 @@ socket.setdefaulttimeout(6)
 #locate your phantomjs
 driver = webdriver.PhantomJS(executable_path='/usr/local/bin/phantomjs')
 
-def getCover(index):
+def getCover(index, minScore):
     driver.get("http://h.bilibili.com/dy" + str(index))
     time.sleep(3)
 
     pageSource = driver.page_source
     bsObj = BeautifulSoup(pageSource, "html.parser")
-    img = bsObj.find("img", {"style":"opacity: 1;"})
 
-    if img is None:
+    scoreObj = bsObj.find("div", {"title": "评分"})
+    if(False == hasScore(scoreObj)):
+        print("no score, return")
         return
-    if img.attrs is None:
-        return
-    if img.attrs["src"] is None:
+    score = getScore(scoreObj)
+
+    if(score < minScore):
+        print("score is less than minScore, return." + str(score) + ":" + str(minScore))
         return
 
+    imgObj = bsObj.find("img", {"style":"opacity: 1;"})
+    if(False == hasImg(imgObj)):
+        print("no img, return")
+        return
+
+    address = getImgAddress(imgObj)
+    print(address)
     try:
-        urlretrieve (img.attrs["src"], str(index)+'.jpg')
+        urlretrieve (address, str(index)+'.jpg')
     except Exception as e:
         print(e)
 
-def getAll(beginIndex, endIndex):
+def hasScore(scoreObj):
+    if scoreObj is None:
+        return False
+    if scoreObj.div is None:
+        return False
+    if scoreObj.div.string is None:
+        return False
+    return True
+
+def getScore(scoreObj):
+    return (int)(scoreObj.div.string);
+
+def hasImg(imgObj):
+    if imgObj is None:
+        return False
+    if imgObj.attrs is None:
+        return False
+    if imgObj.attrs["src"] is None:
+        return False
+    return True
+
+def getImgAddress(imgObj):
+    return imgObj.attrs["src"]
+
+def getAll(beginIndex, endIndex, minScore):
     for i in range(beginIndex,endIndex):
         try:
-            getCover(i)
+            getCover(i, minScore)
         except Exception as e:
             print(e)
 
 first = (int)(argv[1])
 last = (int)(argv[2])
+minScore = (int)(argv[3])
 
-getAll(first,last)
+getAll(first,last,minScore)
--- readme.md
+++ readme.md
@@ -3,11 +3,17 @@ run this to get headers from [here](http://h.bilibili.com/)
     python3 comic.py beginIndex endIndex
 
 Example:
-search headers form http://h.bilibili.com/dy10 to http://h.bilibili.com/dy100:
+search headers from http://h.bilibili.com/dy10 to http://h.bilibili.com/dy100:
 
     python3 comic.py 10 100
 
 --------------------------------
 search covers:
 
-    python3 example.py beginIndex endIndex
+    python3 example.py beginIndex endIndex minScore
+
+
+Example:
+search covers from http://h.bilibili.com/dy10 to http://h.bilibili.com/dy100 which score are higher than 7:
+
+    python3 example.py 10 100 7
